47415ea69887996d4e55ed468027e9203c6ad5d2
N0URELD1N <nom22646@gmail.com>
2025-05-05 13:47:35 +0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,9 +1,1 @@
-    function checkAccess() {
-      const code = document.getElementById("accessCode").value;
-      if (code === "LSPD") {
-        localStorage.setItem("mdtAuth", "granted");
-        window.location.href = "http://localhost:9898/Local/MDT%202/index.html?D660AADA-79C3-403E-956F-D279639F1E47";
-      } else {
-        document.getElementById("error").textContent = "رمز خاطئ. حاول مرة أخرى.";
-      }
-    }
+
